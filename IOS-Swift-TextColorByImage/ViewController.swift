9137c3468643d41939101f7d4f4d1a3892ae0421
Pooya Hatami <pooya@naghmehs-macbook-pro-2.local>
2018-06-21 16:13:51 -0700

    ready

--- IOS-Swift-TextColorByImage/ViewController.swift
+++ IOS-Swift-TextColorByImage/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelView01: UILabel!
+    @IBOutlet weak var labelView02: UILabel!
+    @IBOutlet weak var labelView03: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let image1 = UIImage(named: "colorfull01S") else { return }
+        labelView01.textColor = UIColor(patternImage: image1)
+        guard let image2 = UIImage(named: "colorfullS") else { return }
+        labelView02.textColor = UIColor(patternImage: image2)
+        let image3 = UIImage(named: "colorfull03S")
+        labelView03.textColor = UIColor(patternImage: image3!)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
